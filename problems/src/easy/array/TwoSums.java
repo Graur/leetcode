c792216d1096c010c36c4c700261d6846fa71c41
Graur <ancongr@yandex.ru>
2021-05-04 16:49:36 +0300

    Two Sums

---
+++ problems/src/easy/array/TwoSums.java
@@ -0,0 +1,26 @@
+package easy.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSums {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] {map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}
