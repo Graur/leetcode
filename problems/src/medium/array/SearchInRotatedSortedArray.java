e01d13c6bb2dc4e2e9be662826212ced568259da
Graur <ancongr@yandex.ru>
2021-05-06 14:25:42 +0300

    33. Search in Rotated Sorted Array

---
+++ problems/src/medium/array/SearchInRotatedSortedArray.java
@@ -0,0 +1,13 @@
+package medium.array;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 0)); //4
+        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 3 )); //-1
+        System.out.println(search(new int[]{1}, 0)); //-1
+    }
+
+    public static int search(int[] nums, int target) {
+
+    }
+}
