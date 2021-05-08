caf1f6bf5bef444ba8966abfc125c64b8380a05c
Graur <ancongr@yandex.ru>
2021-05-08 16:29:55 +0300

    169. Majority Element

--- problems/src/easy/array/MajorityElement.java
+++ problems/src/easy/array/MajorityElement.java
@@ -1,7 +1,6 @@
 package easy.array;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class MajorityElement {
     public static void main(String[] args) {
@@ -9,26 +8,7 @@ public class MajorityElement {
     }
 
     public static int majorityElement(int[] nums) {
-        int majority = nums.length/2;
-        Map<Integer, Integer> map = new HashMap();
-        for(int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                int value = map.get(nums[i]);
-                map.put(nums[i], ++ value);
-            } else {
-                map.put(nums[i], 1);
-            }
-        }
-
-        int res = 0;
-        int max = 0;
-        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
-            if (pair.getValue() > max) {
-                max = pair.getValue();
-                res = pair.getKey();
-            }
-        }
-
-        return res;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
