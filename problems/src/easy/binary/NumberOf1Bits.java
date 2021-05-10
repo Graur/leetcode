ceae7e52c26e7722476e98cc0c866407e9437d0d
Graur <ancongr@yandex.ru>
2021-05-10 17:22:48 +0300

    191. Number of 1 Bits

--- problems/src/easy/binary/NumberOf1Bits.java
+++ problems/src/easy/binary/NumberOf1Bits.java
@@ -9,6 +9,14 @@ public class NumberOf1Bits {
 
     // you need to treat n as an unsigned value
     public static int hammingWeight(int n) {
-
+        int mask = 1;
+        int bitsCount = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((mask & n) != 0) {
+                bitsCount++;
+            }
+            mask = mask << 1;
+        }
+        return bitsCount;
     }
 }
