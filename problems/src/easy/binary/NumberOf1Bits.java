5694ce2704e1ee6bdfbd3d32c0d73541ffd8ea8c
Graur <ancongr@yandex.ru>
2021-05-10 17:20:19 +0300

    191. Number of 1 Bits

---
+++ problems/src/easy/binary/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+package easy.binary;
+
+public class NumberOf1Bits {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(00000000000000000000000000001011)); //3
+        System.out.println(hammingWeight(00000000000000000000000010000000)); //1
+//        System.out.println(hammingWeight(11111111111111111111111111111101)); //31
+    }
+
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+
+    }
+}
